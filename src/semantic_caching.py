715cdf9a05eac1e6b2ae25bbf658224618de16e4
Eray Eroğlu <erayeroglu07@gmail.com>
2024-06-27 18:02:21 +0300

    new examples

--- src/semantic_caching.py
+++ src/semantic_caching.py
@@ -90,22 +90,7 @@ class SemanticCache:
 
         return [dict_to_generation(d) for d in json.loads(json_str)]
 
-        
-
-def main():
-    # set environment variables
-    load_dotenv()
-    UPSTASH_VECTOR_REST_URL = os.getenv('UPSTASH_VECTOR_REST_URL')
-    UPSTASH_VECTOR_REST_TOKEN = os.getenv('UPSTASH_VECTOR_REST_TOKEN')
-
-    # initialize Upstash database
-    cache = SemanticCache(url=UPSTASH_VECTOR_REST_URL, token=UPSTASH_VECTOR_REST_TOKEN, min_proximity=0.7)
-    cache.flush()
-    sleep(1)
-    
-    llm = OpenAI(model_name="gpt-3.5-turbo-instruct", n=2, best_of=2)
-    set_llm_cache(cache)
-    
+def example1(llm):
     prompt1 = "Why is the Moon always showing the same side?"
     prompt2 = "How come we always see one face of the moon?"
 
@@ -122,7 +107,26 @@ def main():
     end_time = time.time()
     time_difference = end_time - start_time
     print("Time difference 2:", time_difference, "seconds")
+    
 
+def main():
+    # set environment variables
+    load_dotenv()
+    UPSTASH_VECTOR_REST_URL = os.getenv('UPSTASH_VECTOR_REST_URL')
+    UPSTASH_VECTOR_REST_TOKEN = os.getenv('UPSTASH_VECTOR_REST_TOKEN')
+
+    # initialize Upstash database
+    cache = SemanticCache(url=UPSTASH_VECTOR_REST_URL, token=UPSTASH_VECTOR_REST_TOKEN, min_proximity=0.7)
+    cache.flush()
+    sleep(1)
+    
+    llm = OpenAI(model_name="gpt-3.5-turbo-instruct", n=2, best_of=2)
+    set_llm_cache(cache)
+    
+    example1(llm)
+
+    
+    
 if __name__ == '__main__':
     main()
 
